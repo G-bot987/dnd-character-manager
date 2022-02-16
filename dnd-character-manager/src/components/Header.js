b5d2c85c98c7d564ca33bc480fdf8382a18b289e
Georgia <ghobs987@gmail.com>
2022-02-16 23:24:38 +0000

    manager navigation

---
+++ dnd-character-manager/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>DnD character Manager</h1>
+      <br></br>
+      <a href="#">my characters</a>
+      <br></br>
+      <a href="#">friends </a>
+      <br></br>
+      <a href="#">my games</a>
+      <br></br>
+      <a href="#">profile</a>
+      <br></br>
+    </div>
+  );
+}
