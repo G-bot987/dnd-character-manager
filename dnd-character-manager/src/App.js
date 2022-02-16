5fcd80bd32b0555283a3b9790767132000d330f4
Georgia <ghobs987@gmail.com>
2022-02-16 23:23:16 +0000

    imports on app.js and tabs for card sections

--- dnd-character-manager/src/App.js
+++ dnd-character-manager/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import NavTabs from "./components/NavTabs";
+import Header from "./components/Header";
+import Bio from "./components/pages/Bio";
+import Attributes from "./components/pages/Attributes";
+import Notes from "./components/pages/Notes";
 
 function App() {
+  // use state hook to render different pg's
+
+  const [pg, setCurrentPG] = useState(`bio`);
+
+  const renderPage = () => {
+    if (pg === "bio") {
+      return <Bio />;
+    }
+    if (pg === "attributes") {
+      return <Attributes />;
+    }
+    if (pg === "notes") {
+      return <Notes />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPG(page);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={pg} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
     </div>
   );
 }
