ffc02939564af1a7a15f4c5ee933b8c8de33acde
Georgia <ghobs987@gmail.com>
2022-02-16 23:24:18 +0000

    card components

---
+++ dnd-character-manager/src/components/pages/Attributes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Attributes() {
+  return <p>this is attributes</p>;
+}
---
+++ dnd-character-manager/src/components/pages/Notes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Notes() {
+  return <p>this is notes</p>;
+}
