ec9d03d89db53bdfbdc1c12d8dbed18f0bf596ad
Georgia <ghobs987@gmail.com>
2022-02-17 01:34:57 +0000

    header primary nav display mobile first

--- dnd-character-manager/src/components/Header.js
+++ dnd-character-manager/src/components/Header.js
@@ -4,15 +4,23 @@ export default function Header() {
   return (
     <div>
       <h1>DnD character Manager</h1>
-      <br></br>
-      <a href="#">my characters</a>
-      <br></br>
-      <a href="#">friends </a>
-      <br></br>
-      <a href="#">my games</a>
-      <br></br>
-      <a href="#">profile</a>
-      <br></br>
+      <div className="headercontainer">
+        <div className="headertab">
+          <a href="#">my characters</a>
+        </div>
+
+        <div className="headertab">
+          <a href="#">friends </a>
+        </div>
+
+        <div className="headertab">
+          <a href="#">my games</a>
+        </div>
+
+        <div className="headertab">
+          <a href="#">profile</a>
+        </div>
+      </div>
     </div>
   );
 }
