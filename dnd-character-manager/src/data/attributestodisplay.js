47a585585bd3dffdf0667b40f1f5f6ccb98b4baf
Georgia <ghobs987@gmail.com>
2022-03-11 00:04:40 +0000

    data folder created and attribute sample data

---
+++ dnd-character-manager/src/data/attributestodisplay.js
@@ -0,0 +1,10 @@
+export const attributeInformation = {
+  attributes: [
+    { attname: "Strength" },
+    { attname: "Dexterity" },
+    { attname: "Constitution" },
+    { attname: "Charisma" },
+    { attname: "Inteligence" },
+    { attname: "Wisdom" },
+  ],
+};
