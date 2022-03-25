37bff5f0fa7b462ba372542935358446932a11c7
Georgia <ghobs987@gmail.com>
2022-03-25 20:47:02 +0000

    char info block data

--- dnd-character-manager/src/components/pages/Charinfo.js
+++ dnd-character-manager/src/components/pages/Charinfo.js
@@ -4,8 +4,22 @@ import "./Charinfo.css";
 export default function Charinfo() {
   return (
     <div className="char-info-container">
-      <div>
+      <div className="name-container">
         <p>I am name</p>
+      </div>
+
+      <div className="class-container">
+        <p>class</p>
+      </div>
+
+      <div className="race-container">
+        <p>race</p>
+      </div>
+      <div className="xp-container">
+        <p>xp</p>
+      </div>
+      <div className="alignment-container">
+        <p>alignment</p>
       </div>
     </div>
   );
