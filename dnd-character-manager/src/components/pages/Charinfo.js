f2cab1689b3d549c1fcabd574c074720ac8ee6b0
Georgia <ghobs987@gmail.com>
2022-03-25 20:37:09 +0000

    character info comp

---
+++ dnd-character-manager/src/components/pages/Charinfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Charinfo.css";
+
+export default function Charinfo() {
+  return (
+    <div className="char-info-container">
+      <div>
+        <p>I am name</p>
+      </div>
+    </div>
+  );
+}
