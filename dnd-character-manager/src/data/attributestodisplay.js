5aae38acebdd0012b62a0caac2e933e459f5e53e
Georgia <ghobs987@gmail.com>
2022-03-25 20:36:23 +0000

    data for attributes

--- dnd-character-manager/src/data/attributestodisplay.js
+++ dnd-character-manager/src/data/attributestodisplay.js
@@ -1,9 +1,9 @@
 const attributeInformation = [
-  { attname: "Strength", index: "1" },
-  { attname: "Dexterity", index: "2" },
-  { attname: "Constitution", index: "3" },
-  { attname: "Charisma", index: "4" },
-  { attname: "Inteligence", index: "5" },
-  { attname: "Wisdom", index: "6" },
+  { attname: "Strength" },
+  { attname: "Dexterity" },
+  { attname: "Constitution" },
+  { attname: "Charisma" },
+  { attname: "Inteligence" },
+  { attname: "Wisdom" },
 ];
 export default attributeInformation;
