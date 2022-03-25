17f395111572b16d0aed030d3e1b01c41c40b6a2
Georgia <ghobs987@gmail.com>
2022-03-25 20:38:47 +0000

    char info import

--- dnd-character-manager/src/components/pages/Attributes.js
+++ dnd-character-manager/src/components/pages/Attributes.js
@@ -1,12 +1,16 @@
 import React from "react";
-
 import Attbox from "./Attbox";
+import Charinfo from "./Charinfo";
+import "./Attributes.css";
+
+
 export default function Attributes({ attributeInformation }) {
   console.log(attributeInformation);
 
   return (
-    <div className="attributes container">
+    <div className="card-container">
       <p>this is attributes</p>
+      <Charinfo/>
       <Attbox attributeInformation={attributeInformation} />
     </div>
   );
