6f452c805c8932a25db4c446278d360de3d84ec9
Georgia <ghobs987@gmail.com>
2022-03-26 20:30:50 +0000

    score added to attribute data

--- dnd-character-manager/src/data/attributestodisplay.js
+++ dnd-character-manager/src/data/attributestodisplay.js
@@ -1,9 +1,9 @@
 const attributeInformation = [
-  { attname: "Strength" },
-  { attname: "Dexterity" },
-  { attname: "Constitution" },
-  { attname: "Charisma" },
-  { attname: "Inteligence" },
-  { attname: "Wisdom" },
+  { attname: "Strength", score: 8 },
+  { attname: "Dexterity", score: 8 },
+  { attname: "Constitution", score: 8 },
+  { attname: "Charisma", score: 8 },
+  { attname: "Inteligence", score: 8 },
+  { attname: "Wisdom", score: 8 },
 ];
 export default attributeInformation;
