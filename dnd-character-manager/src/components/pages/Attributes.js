59bdac2b3e162ba1b9226f1b51e6e75167c64e95
Georgia <ghobs987@gmail.com>
2022-03-27 21:55:42 +0100

    avatar fix

--- dnd-character-manager/src/components/pages/Attributes.js
+++ dnd-character-manager/src/components/pages/Attributes.js
@@ -2,16 +2,17 @@ import React from "react";
 import Attbox from "./Attbox";
 import Charinfo from "./Charinfo";
 import "./Attributes.css";
+import img from "../../images/nimrodel.png";
 
 export default function Attributes({ character }) {
   console.log(character.profile.avatar);
-
+  // const img = character.profile.avatar;
   return (
     <div className="card-container">
       <p>this is attributes</p>
       <div
         className="avatar-img"
-        style={{ backgroundImage: `url(${character.profile.avatar})` }}
+        style={{ backgroundImage: `url(${img})` }}
       ></div>
 
       <Charinfo character={character} />
