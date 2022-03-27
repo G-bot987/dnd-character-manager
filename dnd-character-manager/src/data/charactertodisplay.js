9128f33ef1460ba94b473bd20f244c76bae9aa16
Georgia <ghobs987@gmail.com>
2022-03-27 17:45:37 +0100

    charinfo data

--- dnd-character-manager/src/data/charactertodisplay.js
+++ dnd-character-manager/src/data/charactertodisplay.js
@@ -1,6 +1,13 @@
 const character = {
   profile: {
     charName: "Nimrodel",
+    class: "wanderer",
+    lv: 1,
+    race: "Astralende",
+    xp: 200,
+    background: "scion of a fallen house",
+    strength: "loyalty",
+    weakness: "unconcompromising",
   },
   coreStats: [
     { attname: "Strength", score: 8 },
