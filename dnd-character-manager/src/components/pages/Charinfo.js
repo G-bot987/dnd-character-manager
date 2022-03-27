08b34553dbd27b7bfc58b5a507a49336b3b4a334
Georgia <ghobs987@gmail.com>
2022-03-27 17:45:13 +0100

    charinfo render

--- dnd-character-manager/src/components/pages/Charinfo.js
+++ dnd-character-manager/src/components/pages/Charinfo.js
@@ -1,25 +1,38 @@
 import React from "react";
 import "./Charinfo.css";
 
-export default function Charinfo() {
+export default function Charinfo({ character }) {
   return (
     <div className="char-info-container">
       <div className="name-container">
-        <p>I am name</p>
+        <p>I am name:</p>
+        <p>{character.profile.charName}</p>
       </div>
-
+      <div className="xp-container">
+        <p>xp:</p>
+        <p>{character.profile.xp}</p>
+      </div>
+      <div className="xp-container">
+        <p>lv:</p>
+        <p>{character.profile.lv}</p>
+      </div>
       <div className="class-container">
-        <p>class</p>
+        <p>class:</p>
+        <p>{character.profile.class}</p>
       </div>
 
       <div className="race-container">
-        <p>race</p>
+        <p>race:</p>
+        <p>{character.profile.race}</p>
       </div>
-      <div className="xp-container">
-        <p>xp</p>
-      </div>
+
       <div className="alignment-container">
-        <p>alignment</p>
+        <p>background:</p>
+        <p>{character.profile.background}</p>
+        <p>strength:</p>
+        <p>{character.profile.strength}</p>
+        <p>weakness:</p>
+        <p>{character.profile.weakness}</p>
       </div>
     </div>
   );
