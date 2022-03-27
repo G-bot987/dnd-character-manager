5930d113deca8c7de03b25e85d32059bf9e2e0c7
Georgia <ghobs987@gmail.com>
2022-03-27 10:15:48 +0100

    file name change

--- dnd-character-manager/src/data/attributestodisplay.js
+++
@@ -1,9 +0,0 @@
-const attributeInformation = [
-  { attname: "Strength", score: 8 },
-  { attname: "Dexterity", score: 8 },
-  { attname: "Constitution", score: 8 },
-  { attname: "Charisma", score: 8 },
-  { attname: "Inteligence", score: 8 },
-  { attname: "Wisdom", score: 8 },
-];
-export default attributeInformation;
