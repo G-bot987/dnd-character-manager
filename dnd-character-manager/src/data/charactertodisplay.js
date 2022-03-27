020aab74b7f967098f6d0f6192d3f5f773844436
Georgia <ghobs987@gmail.com>
2022-03-27 10:17:05 +0100

    new data structure for all character data

---
+++ dnd-character-manager/src/data/charactertodisplay.js
@@ -0,0 +1,15 @@
+const character = {
+  profile: {
+    charName: "Nimrodel",
+  },
+  coreStats: [
+    { attname: "Strength", score: 8 },
+    { attname: "Dexterity", score: 8 },
+    { attname: "Constitution", score: 8 },
+    { attname: "Charisma", score: 8 },
+    { attname: "Inteligence", score: 8 },
+    { attname: "Wisdom", score: 8 },
+  ],
+};
+
+export default character;
