fd9360c798175a0bbb44b65d7dd931489ef1e0a1
Georgia <ghobs987@gmail.com>
2022-03-27 18:09:16 +0100

    styling for char info

--- dnd-character-manager/src/components/pages/Charinfo.js
+++ dnd-character-manager/src/components/pages/Charinfo.js
@@ -4,29 +4,29 @@ import "./Charinfo.css";
 export default function Charinfo({ character }) {
   return (
     <div className="char-info-container">
-      <div className="name-container">
-        <p>I am name:</p>
+      <div className="quality-container">
+        <p>Name:</p>
         <p>{character.profile.charName}</p>
       </div>
-      <div className="xp-container">
+      <div className="quality-container">
         <p>xp:</p>
         <p>{character.profile.xp}</p>
       </div>
-      <div className="xp-container">
+      <div className="quality-container">
         <p>lv:</p>
         <p>{character.profile.lv}</p>
       </div>
-      <div className="class-container">
+      <div className="quality-container">
         <p>class:</p>
         <p>{character.profile.class}</p>
       </div>
 
-      <div className="race-container">
+      <div className="quality-container">
         <p>race:</p>
         <p>{character.profile.race}</p>
       </div>
 
-      <div className="alignment-container">
+      <div className="background-container">
         <p>background:</p>
         <p>{character.profile.background}</p>
         <p>strength:</p>
