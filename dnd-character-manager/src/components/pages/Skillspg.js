3f4d30e03d61b7de33cc468323b9ea4189a68c3c
Georgia <ghobs987@gmail.com>
2022-04-06 23:40:03 +0100

    added sort by powering attribute for skills

--- dnd-character-manager/src/components/pages/Skillspg.js
+++ dnd-character-manager/src/components/pages/Skillspg.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./Skillspg.css";
 export default function Skillspg({ skillsTemplate }) {
   console.log("skills " + JSON.stringify(skillsTemplate));
+  // sort skills by powering attribute
+  const sorted = skillsTemplate.skills.sort((a, b) =>
+    a.poweringatt.localeCompare(b.poweringatt)
+  );
+  console.log("sorted " + JSON.stringify(sorted));
+
   return (
     <div className="skill-container">
       <ul>
-        {skillsTemplate.skills.map(({ skillname }, index) => (
+        {sorted.map(({ skillname, poweringatt }, index) => (
           <li className="skill-box" id={index} key={index}>
             <p>{skillname}</p>
-
             <span className="dot"></span>
           </li>
         ))}
