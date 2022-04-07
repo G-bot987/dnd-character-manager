17cc10fec4652e93b1f4eee889dbf6be5975d7dd
Georgia <ghobs987@gmail.com>
2022-04-07 12:12:33 +0100

    pushing skills to powring attribute arrays to subsort

--- dnd-character-manager/src/components/pages/Skillspg.js
+++ dnd-character-manager/src/components/pages/Skillspg.js
@@ -8,6 +8,44 @@ export default function Skillspg({ skillsTemplate }) {
   );
   console.log("sorted " + JSON.stringify(sorted));
 
+  // pushing function
+
+  const Charisma = [];
+  const Constitution = [];
+  const Dexterity = [];
+  const Inteligence = [];
+  const Strength = [];
+  const Wisdom = [];
+  const error = [];
+
+  sorted.forEach(function (skill) {
+    console.log(skill);
+    if (skill.poweringatt === "Charisma") {
+      Charisma.push(skill);
+
+      return;
+    } else if (skill.poweringatt === "Constitution") {
+      Constitution.push(skill);
+      return;
+    } else if (skill.poweringatt === "Dexterity") {
+      Dexterity.push(skill);
+      return;
+    } else if (skill.poweringatt === "Inteligence") {
+      Inteligence.push(skill);
+      return;
+    } else if (skill.poweringatt === "Strength") {
+      Strength.push(skill);
+      return;
+    } else if (skill.poweringatt === "Wisdom") {
+      Wisdom.push(skill);
+      return;
+    } else {
+      console.log("error");
+      error.push(skill);
+      return;
+    }
+  });
+
   return (
     <div className="skill-container">
       <ul>
