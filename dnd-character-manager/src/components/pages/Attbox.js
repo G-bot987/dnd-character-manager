6daec2065d86dbfb008a4e52dfc1f6741aae9ec4
Georgia <ghobs987@gmail.com>
2022-04-08 23:00:17 +0100

    use state hook for arrow for attribute bar

--- dnd-character-manager/src/components/pages/Attbox.js
+++ dnd-character-manager/src/components/pages/Attbox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./Attbox.css";
 import Skills from "./Skills";
 
@@ -6,22 +7,29 @@ export default function Attbox({ character, template }) {
   console.log("this is character " + character);
   console.log("this is template attbox " + template);
 
+  const [show, setShow] = useState(false);
+
   console.log("new stats ");
   return (
     <div className="attribute-container">
-      <ul className="list-container">
-        {template.map(({ attribute, skills }, index) => (
-          <div className="attribute-skill-container">
-            <li className="attribute-box" id={index} key={attribute}>
-              {attribute}
-              {console.log(
-                "inside attbox attribute.skills " + JSON.stringify(skills)
-              )}
-            </li>
-            <Skills skills={skills} />
-          </div>
-        ))}
-      </ul>
+      <div>
+        <div className="attribute-arrow" onClick={() => setShow(!show)}></div>
+      </div>
+      {show ? (
+        <ul className="list-container">
+          {template.map(({ attribute, skills }, index) => (
+            <div className="attribute-skill-container">
+              <li className="attribute-box" id={index} key={attribute}>
+                {attribute}
+                {console.log(
+                  "inside attbox attribute.skills " + JSON.stringify(skills)
+                )}
+              </li>
+              <Skills skills={skills} />
+            </div>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
