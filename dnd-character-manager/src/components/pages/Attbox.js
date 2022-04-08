7668ca377e551a384a68158c34ce828b994f51f0
Georgia <ghobs987@gmail.com>
2022-04-08 02:11:58 +0100

    att box using new data and changed to template over char data

--- dnd-character-manager/src/components/pages/Attbox.js
+++ dnd-character-manager/src/components/pages/Attbox.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./Attbox.css";
+import Skills from "./Skills";
 
-export default function Attbox({ character }) {
-  console.log('this is character ' +character);
-  
-  console.log('new stats ' )
+export default function Attbox({ character, template }) {
+  console.log("this is character " + character);
+  console.log("this is template attbox " + template);
+
+  console.log("new stats ");
   return (
     <div className="attribute-container">
       <ul className="list-container">
-        {character.coreStats.map(({ attname, score }, index) => (
-          <li className="attribute-box" id={index} key={index}>
-            {attname} {score}
-
-           
+        {template.map(({ attribute, skills }, index) => (
+          <li className="attribute-box" id={index} key={attribute}>
+            {attribute}
+            {console.log("inside attbox attribute.skills " + JSON.stringify(skills))}
+            <Skills skills={skills} />
           </li>
         ))}
       </ul>
     </div>
   );
 }
+
+// {character.coreStats.map(({ attname, score }, index) => (
+//   <li className="attribute-box" id={index} key={index}>
+//     {attname} {score}
+
+//   </li>
