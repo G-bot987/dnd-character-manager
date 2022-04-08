1f26c0fee1f735be393f0b75294973f0eca7d474
Georgia <ghobs987@gmail.com>
2022-04-09 00:50:35 +0100

    styling for cirlce arround arrow and key error with list render fix

--- dnd-character-manager/src/components/pages/Attbox.js
+++ dnd-character-manager/src/components/pages/Attbox.js
@@ -12,14 +12,21 @@ export default function Attbox({ character, template }) {
   console.log("new stats ");
   return (
     <div className="attribute-container">
-      <div>
-        <div className="attribute-arrow" onClick={() => setShow(!show)}></div>
+      <div className="cirlce">
+        <div
+          className="attribute-arrow"
+          onClick={() => setShow(!show)}
+          // style={{
+          //   transistion: "all 0.5s ease",
+          //   transform: 'rotate(${show ? 0 : "135deg"})',
+          // }}
+        ></div>
       </div>
       {show ? (
         <ul className="list-container">
           {template.map(({ attribute, skills }, index) => (
-            <div className="attribute-skill-container">
-              <li className="attribute-box" id={index} key={attribute}>
+            <div className="attribute-skill-container" key={attribute}>
+              <li className="attribute-box" id={index}>
                 {attribute}
                 {console.log(
                   "inside attbox attribute.skills " + JSON.stringify(skills)
