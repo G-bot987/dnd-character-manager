df729f6c4fa7b5e8d0b559869af68920b1c826cb
Georgia <ghobs987@gmail.com>
2022-04-08 22:58:57 +0100

    use state hook for skills arrow dropdown

--- dnd-character-manager/src/components/pages/Skills.js
+++ dnd-character-manager/src/components/pages/Skills.js
@@ -1,23 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import "./Skills.css";
+
 export default function Skills({ skills }) {
   console.log("skills comp " + JSON.stringify(skills));
 
   // sort skills alphabetically
   const sorted = skills.sort((a, b) => a.skillname.localeCompare(b.skillname));
   console.log("sorted " + JSON.stringify(sorted));
-
+  const [show, setShow] = useState(false);
   return (
-    
-      <ul className="skill-list">
-        <p>Skills</p>
-        {sorted.map(({ skillname, poweringatt }, index) => (
-          <li className="skill-box" id={index} key={index}>
-            <p>{skillname}</p>
-            <span className="dot"></span>
-          </li>
-        ))}
-      </ul>
-    
+    sorted.length > 0 && (
+      <div>
+        <div className="skill-arrow" onClick={() => setShow(!show)}></div>
+        {show ? (
+          <ul className="skill-list">
+            <p>skills</p>
+            {sorted.map(({ skillname, poweringatt }, index) => (
+              <li className="skill-box" id={index} key={index}>
+                <p>{skillname}</p>
+                <span className="dot"></span>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
+    )
   );
 }
