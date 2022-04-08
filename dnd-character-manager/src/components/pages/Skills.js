f3324c98ae7cd5ed6bc53430bfea3797d4bae965
Georgia <ghobs987@gmail.com>
2022-04-08 12:29:04 +0100

    refracting skills comp

--- dnd-character-manager/src/components/pages/Skills.js
+++ dnd-character-manager/src/components/pages/Skills.js
@@ -2,67 +2,15 @@ import React from "react";
 import "./Skills.css";
 export default function Skills({ skills }) {
   console.log("skills comp " + JSON.stringify(skills));
-  // console.log("template " + JSON.stringify(template));
-  // // sort skills alphabetically
-  // const sorted = template.skills.sort((a, b) =>
-  //   a.skillname.localeCompare(b.skillname)
-  // );
-  // console.log("sorted " + JSON.stringify(sorted));
 
-  // // pushing function
-
-  // const Charisma = [];
-  // const Constitution = [];
-  // const Dexterity = [];
-  // const Inteligence = [];
-  // const Strength = [];
-  // const Wisdom = [];
-  // const error = [];
-  // // push skills to array based on powering attribute
-  // sorted.forEach(function (skill) {
-  //   console.log(skill);
-  //   if (skill.poweringatt === "Charisma") {
-  //     Charisma.push(skill);
-
-  //     return;
-  //   } else if (skill.poweringatt === "Constitution") {
-  //     Constitution.push(skill);
-  //     return;
-  //   } else if (skill.poweringatt === "Dexterity") {
-  //     Dexterity.push(skill);
-  //     return;
-  //   } else if (skill.poweringatt === "Inteligence") {
-  //     Inteligence.push(skill);
-  //     return;
-  //   } else if (skill.poweringatt === "Strength") {
-  //     Strength.push(skill);
-  //     return;
-  //   } else if (skill.poweringatt === "Wisdom") {
-  //     Wisdom.push(skill);
-  //     return;
-  //   } else {
-  //     console.log("error");
-  //     error.push(skill);
-  //     return;
-  //   }
-  // });
-  // // after skills pushed to array based on powering att, they are then ordered alphabetically.
-
-  // // sorted alphatetically ordered skills are then pushed to one array to be mapped over in component.
-  // const sortedSkills = Charisma.concat(
-  //   Constitution,
-  //   Dexterity,
-  //   Inteligence,
-  //   Strength,
-  //   Wisdom
-  // );
-
-  // console.log("sorted skills " + JSON.stringify(sortedSkills));
+  // sort skills alphabetically
+  const sorted = skills.sort((a, b) => a.skillname.localeCompare(b.skillname));
+  console.log("sorted " + JSON.stringify(sorted));
 
   return (
     <div className="skill-container">
       <ul>
-        {skills.map(({ skillname, poweringatt }, index) => (
+        {sorted.map(({ skillname, poweringatt }, index) => (
           <li className="skill-box" id={index} key={index}>
             <p>{skillname}</p>
             <span className="dot"></span>
