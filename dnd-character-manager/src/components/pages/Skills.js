88a1b63879662fef5f733c63caefe1b8e8efa668
Georgia <ghobs987@gmail.com>
2022-04-10 16:15:11 +0100

    removing sort and syntax chanage

--- dnd-character-manager/src/components/pages/Skills.js
+++ dnd-character-manager/src/components/pages/Skills.js
@@ -3,14 +3,9 @@ import { useState } from "react";
 import "./Skills.css";
 
 export default function Skills({ skills }) {
-  console.log("skills comp " + JSON.stringify(skills));
-
-  // sort skills alphabetically
-  const sorted = skills.sort((a, b) => a.skillname.localeCompare(b.skillname));
-  console.log("sorted " + JSON.stringify(sorted));
   const [show, setShow] = useState(false);
   return (
-    sorted.length > 0 && (
+    skills.length > 0 && (
       <div>
         <div className="cirlce">
           <div className="skill-arrow" onClick={() => setShow(!show)}></div>
@@ -18,7 +13,7 @@ export default function Skills({ skills }) {
         {show ? (
           <ul className="skill-list">
             <p>skills</p>
-            {sorted.map(({ skillname, poweringatt }, index) => (
+            {skills.map(({ skillname, poweringatt }, index) => (
               <li className="skill-box" id={index} key={index}>
                 <p>{skillname}</p>
                 <span className="dot"></span>
