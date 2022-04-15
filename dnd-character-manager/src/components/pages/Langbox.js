3680acb1faa415f3386435ac0d0cb60f966f6482
Georgia <ghobs987@gmail.com>
2022-04-15 17:53:51 +0100

    obj.entries use to create array with inherited properties

--- dnd-character-manager/src/components/pages/Langbox.js
+++ dnd-character-manager/src/components/pages/Langbox.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import "./langbox.css";
+import Language from "./Language";
+
 export default function Attributes({ languages }) {
-  console.log("langbox " + JSON.stringify(languages, null, 2));
   const [show, setShow] = useState(false);
+
+  const languagesarray = Object.entries(languages);
+  console.log("obj.entries" + JSON.stringify(languagesarray, null, 2));
   return (
     <div className="language-container">
       {!show ? <p className="lang-title">Languages</p> : null}
@@ -16,6 +20,23 @@ export default function Attributes({ languages }) {
           }}
         ></div>
       </div>
+      {show ? (
+        <ul className="list-container">
+          {languagesarray.map(
+            ({ languagegroup }, index) => (
+              console.log(languagegroup),
+              (
+                <div className="attribute-skill-container" key={index}>
+                  <li className="attribute-box" id={languagegroup}>
+                    {languagegroup}
+                  </li>
+                  <Language languagegroup={languagegroup} />
+                </div>
+              )
+            )
+          )}
+        </ul>
+      ) : null}
     </div>
   );
 }
