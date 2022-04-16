fb221b0b0b3718cf2e259a75efcf60e91130d1c7
Georgia <ghobs987@gmail.com>
2022-04-16 21:31:29 +0100

    languagedetails comp for language details, template only

---
+++ dnd-character-manager/src/components/pages/Languagedetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Language from './Language'
+
+const Languagedetail = (Language) => {
+    console.log('language detail ', Language)
+  return (
+    <div>Languagedetail</div>
+  )
+}
+
+export default Languagedetail
