5244dfd0a976f91f918fa3049434fb138a342087
Georgia <ghobs987@gmail.com>
2022-04-17 22:06:31 +0100

    language details with conditional render for dialects if array exists

--- dnd-character-manager/src/components/pages/Languagedetail.js
+++ dnd-character-manager/src/components/pages/Languagedetail.js
@@ -10,26 +10,66 @@ export default function Languagedetail({
   proficiency,
   dialect,
 }) {
-  console.log(dialect);
+  const [show, setShow] = useState(false);
+  if (dialect === undefined) {
+    dialect = [];
+  } else {
+  }
   return (
     <div className="language-detail-box">
-      <div className="row-language-details">
-        <ul className="language-details-list">
-          <li className="language-details-list-item">
-            proficiency{proficiency}
-          </li>
-          <li className="language-details-list-item">
-            {" "}
-            inflection {inflection}
-          </li>
-        </ul>
+      <div className="first-row">
+        <div className="row-language-details">
+          <ul className="language-details-list">
+            <li className="language-details-list-item">
+              proficiency{proficiency}
+            </li>
+            <li className="language-details-list-item">
+              inflection {inflection}
+            </li>
+          </ul>
+        </div>
+        <div className="row-language-details">
+          <ul className="language-details-list">
+            <li className="language-details-list-item"> spoken {spoken}</li>
+            <li className="language-details-list-item"> written{written}</li>
+          </ul>
+        </div>
       </div>
-      <div className="row-language-details">
-        <ul className="language-details-list">
-          <li className="language-details-list-item"> spoken {spoken}</li>
-          <li className="language-details-list-item"> written{written}</li>
-        </ul>
-      </div>
+      {dialect.length > 0 && (
+        <div className="dialect-dropdown">
+          <div
+            className="dialects-arrow-container"
+            style={{
+              marginTop: show ? "0.3rem " : "",
+            }}
+          >
+            <div
+              className="dialects-arrow-down"
+              onClick={() => setShow(!show)}
+              style={{
+                transform: show ? "rotate(225deg)" : "",
+                transition: "transform 150ms ease",
+              }}
+            ></div>
+          </div>
+          dialects
+          <div
+            className="dialects-arrow-container"
+            style={{
+              marginTop: show ? "0.3rem " : "",
+            }}
+          >
+            <div
+              className="dialects-arrow-down"
+              onClick={() => setShow(!show)}
+              style={{
+                transform: show ? "rotate(225deg)" : "",
+                transition: "transform 150ms ease",
+              }}
+            ></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
