8c7d0dd81b10685cb39ded7978b2cd9e364d7263
Georgia <ghobs987@gmail.com>
2022-04-17 14:15:57 +0100

    lang details comp structure in line with css flex row for 2 collums of ul, rendering language details

--- dnd-character-manager/src/components/pages/Languagedetail.js
+++ dnd-character-manager/src/components/pages/Languagedetail.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "./Skills.css";
+import "./languagedetails.css";
 
 export default function Languagedetail({ language, inflection }) {
   console.log(inflection);
   return (
-    <div>
-      {language} {inflection}
+    <div className="language-detail-box">
+      <div className="row-language-details">
+        <ul className="language-details-list">
+          <li className="language-details-list-item">
+            {language} {inflection}
+          </li>
+          <li className="language-details-list-item">{language}</li>
+        </ul>
+      </div>
+      <div className="row-language-details">
+        <ul className="language-details-list">
+          <li className="language-details-list-item">
+            {language} {inflection}
+          </li>
+          <li className="language-details-list-item">{language}</li>
+        </ul>
+      </div>
     </div>
   );
 }
