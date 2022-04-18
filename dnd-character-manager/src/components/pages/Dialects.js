b37fcfda709e840c5f28fbdc9f8357e7189fee79
Georgia <ghobs987@gmail.com>
2022-04-18 22:57:47 +0100

    dialects btn and styling

--- dnd-character-manager/src/components/pages/Dialects.js
+++ dnd-character-manager/src/components/pages/Dialects.js
@@ -3,9 +3,23 @@ import { useState, useEffect } from "react";
 import "./dialects.css";
 
 export default function Dialects({ version, index }) {
+  const [skillsSelected, setSkillsSelected] = useState(() => {
+    const saved = localStorage.getItem(version.proficiency);
+    const initialValue = JSON.parse(saved);
+    return initialValue || false;
+  });
   return (
     <li className="dialect-box" key={index}>
       {version.name} proficiency{version.proficiency}
+      <div
+        id={version.proficiency}
+        onClick={() => setSkillsSelected(!skillsSelected)}
+        className="dialect-detail-dot"
+        style={{
+          backgroundColor: skillsSelected ? "red" : "",
+          transition: "transform 150ms ease",
+        }}
+      ></div>
     </li>
   );
 }
